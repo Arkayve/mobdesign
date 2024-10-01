0e51edec45640cffb13beacbcc376e73b3ea6421
arkayve <fwchartier@gmail.com>
2024-10-01 12:24:29 +0200

    ✨ sass, burger menu, index.html

---
+++ src/assets/js/components/_header__burger.js
@@ -0,0 +1,10 @@
+export function manageBurger() {
+    const header__burger = document.getElementById('header__burger');
+    const header__menu = document.getElementById('header__menu');
+
+    // Ajouter ou retirer la classe "open" sur le clic du bouton burger
+    header__burger.addEventListener('click', () => {
+    header__burger.classList.toggle('open');  // Animation du burger en croix
+    header__menu.classList.toggle('open');    // Afficher/masquer le menu
+    });
+}
--- src/view/productView.js
+++ src/view/productView.js
@@ -1,12 +1,14 @@
 export function renderProducts(products) {
     const productList = document.getElementById('product-list');
     products.forEach(product => {
-      const productElement = document.createElement('div');
-      productElement.innerHTML = `
-        <h2>${product.name}</h2>
-        <p>Prix : ${product.price}€</p>
-        <button onclick="addToCart(${product.id})">Ajouter au panier</button>
-      `;
-      productList.appendChild(productElement);
-    });
+        const productTemplate = document.getElementById('productTemplate')
+        const productContainer = document.importNode(productTemplate.content, true)
+        productContainer.querySelector('.js-product__img').src = product.imageUrl
+        productContainer.querySelector('.js-product__title').textContent = product.title
+        productContainer.querySelector('.js-product__price').textContent = product.price.toString().replace(".", "€")
+        productContainer.querySelector('.js-product__category').textContent = product.category
+        productContainer.querySelector('.js-product__description').textContent = product.description
+        productContainer.querySelector('.js-product__button').textContent = 'Découvrir'
+        document.getElementById('main').appendChild(productContainer)
+    });  
 }
